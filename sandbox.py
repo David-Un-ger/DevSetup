6fa870c1220067d6f1737132b66f2c3cbf386784
David Unger <david.unger.de@gmail.com>
2022-12-30 15:00:14 +0000

    devcontainer working - black&flake8 not yet

--- sandbox.py
+++ sandbox.py
@@ -1,13 +1,31 @@
 # %%
 
-import numpy as np
-import toml#lib 
-from box import Box
-import torch
 import pathlib
+
+import numpy as np
+import toml  # lib
+import torch
+from box import Box
 
 path = pathlib.Path("config.toml")
 config = pathlib.Path.read_text(path)
 config = Box(toml.loads(config))
 # %%
 print("Cuda available: ", torch.cuda.is_available())
+print(
+    "this a a test lafjdöljföl j ölkjaölkj ölkjaö lkj ölja ölakjölkjalöjfölfkjöl kjaölk jaölj ölakj öl kjöl kjkälfsööjfjkölf fdfff fdf  f j l j lkjglkjgölgjgjljggglgkgjklgkjgjklgöjkfgljslgkjsgjölk"
+)
+
+for dlkjalköjfaöljf_fojflfkjff in range(3893893893893838):
+    print( 289892892822838338839,
+        "djlkfjfkljfflökfjlkfjkl",  "lk jdlkdfjflöfjfölkfjlökjsdölkjflöfkj",     12222222,  "djlkffklfjfölfjdfjl", 11222, 3333,44444
+    )
+
+
+
+i = 5
+a, b = 2,3333
+print(22333)
+
+
+print("djdlkfjflfkj")
